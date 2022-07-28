6a61e4dd8547dbd13b31c1a77917bf727b8aac11
Muhammad Falah A <92344349+falahdrrhmn@users.noreply.github.com>
2022-07-28 22:45:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// $(window).scroll(function () {
+//     var wScroll = $(this).scrollTop();
+
+//     console.log(wScroll);
+//     if (wScroll >= 0 && wScroll <= 250) {
+//         $('body').removeClass('bg2').addClass('bg1');
+//     } else {
+//         $('body').removeClass('bg1').addClass('bg2');
+
+//     }
+// });
